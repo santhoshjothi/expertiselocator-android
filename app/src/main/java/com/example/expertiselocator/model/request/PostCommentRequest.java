55827ffb9e6d9b632561c3a75cfb8fe24e940cd8
rajasekard <rajasekar@salzertechnologies.com>
2019-01-09 13:19:57 +0530

    Code Updated - PostCommentRequest class added

---
+++ app/src/main/java/com/example/expertiselocator/model/request/PostCommentRequest.java
@@ -0,0 +1,46 @@
+package com.example.expertiselocator.model.request;
+
+public class PostCommentRequest {
+
+    private String postID, userID, comments, commentedBy, modifiedBy;
+
+    public String getPostID() {
+        return postID;
+    }
+
+    public void setPostID(String postID) {
+        this.postID = postID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getCommentedBy() {
+        return commentedBy;
+    }
+
+    public void setCommentedBy(String commentedBy) {
+        this.commentedBy = commentedBy;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+}
